03e6bf8ffadd09024d53e744cd70c218bafe486c
Max Dubecky <mdubecky@gmail.com>
2012-09-01 21:41:31 -0400

    initial commit with github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApprenticeFarmers::Application.config.secret_token = '435f0c763bd2f8ec60cbe4f042b8f9058f3821a0b32840ae347f81b0b618d3fedb7cd50abf347beb9ceac6eabc4237c2940982275fc01a67fc165b74872d758d'
